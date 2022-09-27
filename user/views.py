cb33e4096b8f833c8649f932fdc10a84738d2bf3
Milan Sony <milanfrom2001@gmail.com>
2022-09-27 18:58:05 +0530

    user signup form password hashed

--- user/views.py
+++ user/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from .models import CustomUser as user
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.hashers import make_password
 from django.http import HttpResponse
 
 # Create your views here.
@@ -18,7 +20,7 @@ def usersignup(request):
         messages.warning(request, "This Email is already exist")
         return redirect('usersignuppage')
       else:
-        user(name=name, email=email, password=password).save()
+        user(name=name, email=email, password=make_password(password)).save()
         messages.success(request, "Your accout has been successfully created")
         return redirect('userloginpage')
     else:
@@ -30,4 +32,15 @@ def usersignup(request):
 #! User login
 
 def userlogin(request):
-  pass
+  if request.method == 'POST':
+    email = request.POST['email']
+    password = request.POST['password']
+    user = authenticate(request, email=email, password=password)
+    if user is not None:
+      login(request, user)
+      return render(request, "userlists.html")
+    else:
+      messages.warning(request, "Check you username and password")
+      return redirect('userlogin')
+  else:
+    return render(request, "userlogin.html")
