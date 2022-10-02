1c24a4c176943ab889d72012199f23232ad96887
Milan Sony <milanfrom2001@gmail.com>
2022-10-02 23:02:24 +0530

    todolist data added into database from user via form

--- todolist/views.py
+++ todolist/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import messages
 from todolist.forms import todoform
 
 # Create your views here.
@@ -7,5 +8,24 @@ def userhome_todo(request):
   form = todoform()
   return render(request, "userhomepage.html", {'todoform':form})
 
+
+
 def addtodolist(request):
-  pass
+  if request.user.is_authenticated:
+    user = request.user
+    print(user)
+    form = todoform(request.POST)
+    if form.is_valid():
+      print(form.cleaned_data)
+      todolist = form.save(commit=False) 
+      # Saving with commit=False gets you a model object, then you can add your extra data and save it
+      # https://stackoverflow.com/questions/12848605/django-modelform-what-is-savecommit-false-used-for
+      todolist.user = user
+      todolist.save()
+      print(todolist)
+      messages.warning(request, "Your list is added")
+      return redirect('todolist:userhome_todo')
+    else:
+      messages.warning(request, "Something went wrong please try again")
+      return redirect('todolist:userhome_todo')
+
