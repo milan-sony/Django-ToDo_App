841320c299038b2177d07cf28600218408730afb
Milan Sony <milanfrom2001@gmail.com>
2022-10-16 14:11:33 +0530

    todolist encrypted using django-cryptography link: https://github.com/georgemarshall/django-cryptography | https://pypi.org/project/django-cryptography/

--- todolist/models.py
+++ todolist/models.py
@@ -1,11 +1,13 @@
 from django.db import models
-
 from user.models import CustomUser
+from django_cryptography.fields import encrypt
+# https://github.com/georgemarshall/django-cryptography
+# https://pypi.org/project/django-cryptography/
 
 # Create your models here.
 class todolists(models.Model):
   user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-  todo = models.CharField(max_length=255)
+  todo = encrypt(models.CharField(max_length=255))
   todo_completed = models.BooleanField(default=False)
   created_date = models.DateField(auto_now_add=True)
 
